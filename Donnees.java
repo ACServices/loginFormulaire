5518c214a8ebb71e854765d26a0a916d34e84eb7
Ismael Bin Moussa <ismaelbinmoussa@gmail.com>
2020-04-29 14:44:29 +0300

    ajout de la classe Donnees

--- Donnees.java
+++ Donnees.java
@@ -3,7 +3,7 @@ package formulaireProject;
 import javax.swing.JTextArea;
 
 public class Donnees extends JTextArea{
-
+//
 	public Donnees() {}
 	public Donnees(String text) {
 		this.setText(text);
